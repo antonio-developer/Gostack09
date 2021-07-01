5296152790a3f0677ef9efa915fc0372bcef0a5f
Antonio <antoniodeveloper3@gmail.com>
2021-07-01 19:13:24 -0300

    Dominando o NodeJS

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const server = express();
+
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = {"name":"Diego", "email":"diego@rocketseat.com.br"}
+
+const users = ['Diego', 'Cláudio', 'Victor'];
+
+server.get('/users/:index',  (req, res)=>{
+  const {index} = req.params;
+
+  return res.json(users[index]);
+})
+
+server.listen(3000);
